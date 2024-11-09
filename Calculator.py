74bd857dca349e8291109b9e0266758d50044803
myadav <myadav@PUN756796.local>
2024-11-09 11:24:15 +0530

    Add remainder calculation

--- Calculator.py
+++ Calculator.py
@@ -9,12 +9,14 @@ def add(a, b):
 #Multiplication - Kalyan
 #division - Rajesh
 #Add Remainder - Manisha
-
+def remainder(a, b):
+    return a % b 
 
 @app.route('/Calculator/<float:n1>/<float:n2>')
 def result(n1, n2):
     results = {
         'add': add(n1, n2),
+        'remainder': remainder(n1, n2)
         
     }
     return jsonify(results)
