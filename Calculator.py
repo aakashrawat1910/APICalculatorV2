6005932962e18b5bb12c5e9dd36aff8747635694
Aakash <Aakashrawat1910@gmail.com>
2024-11-09 10:59:35 +0530

    main

---
+++ Calculator.py
@@ -0,0 +1,25 @@
+from flask import Flask,request,jsonify
+
+app=Flask(__name__)
+
+def add(a, b):
+    return a + b
+
+#Sub - Reshma
+#Multiplication - Kalyan
+#division - Rajesh
+#Add Remainder - Manisha
+
+
+@app.route('/Calculator/<float:n1>/<float:n2>')
+def result(n1, n2):
+    results = {
+        'add': add(n1, n2),
+        
+    }
+    return jsonify(results)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
